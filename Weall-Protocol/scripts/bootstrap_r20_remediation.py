1e0aa0441c26d7280d2bcadb661514ca653424f4
errol1swaby2-bit <errol1swaby2@gmail.com>
2026-09-23 07:26:40 -0700

    fix(r20): register pin not found failure

--- Weall-Protocol/scripts/bootstrap_r20_remediation.py
+++ Weall-Protocol/scripts/bootstrap_r20_remediation.py
@@ -15,6 +15,7 @@ EXTRA_FAILURE_IDS = {
     'invalid_payload:self_transfer_forbidden': 'FAIL-5B5956453E3B2F2E',
     'invalid_tx:missing_key_id': 'FAIL-B57D98F1582AA8F4',
     'invalid_tx:session_ttl_s_must_be_positive': 'FAIL-D40A4DAB3F037D44',
+    'not_found:pin_not_found': 'FAIL-8DC2F2AAA82257FB',
 }
 
 _HELPER = '''
